9bed0c5c2438818d9a328c1e0bb2334a3709a54f
MathieuRaynaud <mathieu-006@hotmail.fr>
2018-12-15 17:06:22 +0100

    Added the main R file

---
+++ main.R
@@ -0,0 +1,4 @@
+# The place of sports in the society
+
+# Analyse of an open data set from the french government, inventorying the sportive infrastructures in France since 2012
+# Link of the data set: https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.data.gouv.fr%2Fs%2Fresources%2Frecensement-des-equipements-sportifs-espaces-et-sites-de-pratiques%2F20170817-165944%2FRES_T2_20122016.zip%3Ffbclid%3DIwAR161Lr8fZ6z_ri0LPXBgB3s6fJ8ukpIgwpnddm9f9CfWdAvpgp2CrkiNcQ&h=AT21de_y4OuGqsqaCbUAQl9RJe7nsBD9dvf4-ftbFbnD8QjhKPLwoVBNkZNX1H7cBxmqKdbx0BqNSdnnARIu0JEuY0ru9n1DOa82Xwlkv0L88HHt-6WPP-6FHK4DtrkYxtU
