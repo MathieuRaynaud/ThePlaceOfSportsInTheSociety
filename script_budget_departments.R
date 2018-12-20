7e62f11ab99423bf4a8c7b82d6c3963713fdcf07
MathieuRaynaud <mathieu-006@hotmail.fr>
2018-12-20 17:53:06 +0100

    Upgraded the way of finding the investment in departments by swiping those for which we do not have data at all

--- script_budget_departments.R
+++ script_budget_departments.R
@@ -13,6 +13,10 @@ Budget_per_department_2014[is.na(Budget_per_department_2014)] <- 0
 Budget_per_department_2014$Investment_budget <- Budget_per_department_2014$Service_investment + Budget_per_department_2014$Collectivity_investment
 ggplot(data=Budget_per_department_2014, aes(x = DepCode, y = Investment_budget, fill=DepCode)) + geom_bar(stat='identity', position='identity', show.legend=FALSE)
 
+#Remove rows for which data is missing (investment = 0)
+Unknown_departments <- Budget_per_department_2014[c(which(Budget_per_department_2014$Investment_budget == 0)), ]
+Budget_per_department_2014 <- Budget_per_department_2014[-c(which(Budget_per_department_2014$Investment_budget == 0)), ]
+
 #10 highest investements
 highest_investments <- head(Budget_per_department_2014[order(Budget_per_department_2014$Investment_budget, decreasing=TRUE), ], 10)
 ggplot(data=highest_investments, aes(x=reorder(DepCode, -Investment_budget), y=Investment_budget, fill=DepName)) + geom_col(position = 'dodge')
@@ -20,3 +24,6 @@ ggplot(data=highest_investments, aes(x=reorder(DepCode, -Investment_budget), y=I
 #10 lower investments
 lower_investments <- tail(Budget_per_department_2014[order(Budget_per_department_2014$Investment_budget, decreasing=TRUE), ], 10)
 ggplot(data=lower_investments, aes(x=reorder(DepCode, Investment_budget), y=Investment_budget, fill=DepName)) + geom_col(position = 'dodge')
+
+## BE CAREFUL !!! WE DO NOT HAVE DATA FOR THESE DEPARTMENTS
+View(Unknown_departments)
