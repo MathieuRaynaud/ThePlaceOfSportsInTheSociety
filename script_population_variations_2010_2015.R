2280b41298221323359a07ae37b6dd6277041a96
MathieuRaynaud <mathieu-006@hotmail.fr>
2018-12-21 12:35:00 +0100

    Replaced png files by pdf

--- script_population_variations_2010_2015.R
+++ script_population_variations_2010_2015.R
@@ -7,12 +7,12 @@ library("readr", lib.loc="~/R/win-library/3.5")
 Population_variations_2010_2015 = read.csv("/Users/Mathieu/Documents/INSA/5ème année/Traitement et Analyse des Données pour le Big Data/Projet/ThePlaceOfSportsInTheSociety/Population_variations_2010_2015.csv", sep=";")
 
 #Plot data
-ggplot(data=Population_variations_2010_2015, aes(x=DepCode, y=Variation, fill=DepCode)) + geom_bar(stat='identity', position='identity',show.legend = FALSE)
+ggplot(data=Population_variations_2010_2015, aes(x=DepCode, y=Variation, fill=DepCode)) + ylab("Average variation per year (%)") + xlab("Department code") + geom_bar(stat='identity', position='identity',show.legend = FALSE)
 
 #10 most attractive departments
 most_attractives <- head(Population_variations_2010_2015[order(Population_variations_2010_2015$Variation, decreasing=TRUE), ], 10)
-ggplot(data=most_attractives, aes(x=reorder(DepCode, -Variation), y=Variation, fill=DepName)) + geom_col(position = 'dodge')
+ggplot(data=most_attractives, aes(x=reorder(DepCode, -Variation), y=Variation, fill=DepName)) + ylab("Average variation per year (%)") + xlab("Department code") + geom_col(position = 'dodge')
 
 #10 less attractive departments
 less_attractives <- tail(Population_variations_2010_2015[order(Population_variations_2010_2015$Variation, decreasing=TRUE), ], 10)
-ggplot(data=less_attractives, aes(x=reorder(DepCode, Variation), y=Variation, fill=DepName)) + geom_col(position = 'dodge')
+ggplot(data=less_attractives, aes(x=reorder(DepCode, Variation), y=Variation, fill=DepName)) + ylab("Average variation per year (%)") + xlab("Department code") + geom_col(position = 'dodge')
