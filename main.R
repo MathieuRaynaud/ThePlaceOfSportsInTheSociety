55363b4b6ab134300302aebf18ead8a8cd88b204
MathieuRaynaud <mathieu-006@hotmail.fr>
2018-12-21 12:17:51 +0100

    Corrected department investments

--- main.R
+++ main.R
@@ -1,8 +1,7 @@
 # The place of sports in the society
 
 # Analyse of an open data set from the french government, inventorying the sportive infrastructures in France since 2012
-# Link of the data set: https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.data.gouv.fr%2Fs%2Fresources%2Frecensement-des-equipements-sportifs-espaces-et-sites-de-pratiques%2F20170817-165944%2FRES_T2_20122016.zip%3Ffbclid%3DIwAR161Lr8fZ6z_ri0LPXBgB3s6fJ8ukpIgwpnddm9f9CfWdAvpgp2CrkiNcQ&h=AT21de_y4OuGqsqaCbUAQl9RJe7nsBD9dvf4-ftbFbnD8QjhKPLwoVBNkZNX1H7cBxmqKdbx0BqNSdnnARIu0JEuY0ru9n1DOa82Xwlkv0L88HHt-6WPP-6FHK4DtrkYxtU
-
+# Link of the data set: https://www.data.gouv.fr/s/resources/recensement-des-equipements-sportifs-espaces-et-sites-de-pratiques/20170817-165944/RES_T2_20122016.zip
 #Use libraries
 library("ggplot2", lib.loc="~/R/win-library/3.5")
 library("plyr", lib.loc="~/R/win-library/3.5")
--- script_budget_departments.R
+++ script_budget_departments.R
@@ -4,32 +4,24 @@ library("plyr", lib.loc="~/R/win-library/3.5")
 library("readr", lib.loc="~/R/win-library/3.5")
 
 #Import data
-Budget_per_department_2014 = read.csv("/Users/Mathieu/Documents/INSA/5ème année/Traitement et Analyse des Données pour le Big Data/Projet/ThePlaceOfSportsInTheSociety/Budget_per_department_2014.csv", sep=";")
-
-#Replace N.A values by 0
-Budget_per_department_2014[is.na(Budget_per_department_2014)] <- 0
-
-#Compute total investment budget
-Budget_per_department_2014$Investment_budget <- Budget_per_department_2014$Service_investment + Budget_per_department_2014$Collectivity_investment
-
-#Remove rows for which data is missing (investment = 0)
-Unknown_departments <- Budget_per_department_2014[c(which(Budget_per_department_2014$Investment_budget == 0)), ]
-Budget_per_department_2014 <- Budget_per_department_2014[-c(which(Budget_per_department_2014$Investment_budget == 0)), ]
+Population_per_department_2015 = read.csv("/Users/Mathieu/Documents/INSA/5ème année/Traitement et Analyse des Données pour le Big Data/Projet/ThePlaceOfSportsInTheSociety/Population_per_department_2015.csv", sep = ";")
+Budget_per_department_2015 = read.csv("/Users/Mathieu/Documents/INSA/5ème année/Traitement et Analyse des Données pour le Big Data/Projet/ThePlaceOfSportsInTheSociety/Depenses_InvestissementmentAllDept2015.csv", sep=";")
+Budget_per_department_2015 <- merge(Budget_per_department_2015, Population_per_department_2015, by = "DepName")
 
 #Plot investment per department
-ggplot(data=Budget_per_department_2014, aes(x = DepCode, y = Investment_budget, fill=DepCode)) + geom_bar(stat='identity', position='identity', show.legend=FALSE)
+ggplot(data=Budget_per_department_2015, aes(x = DepCode, y = Depensestotales, color=DepCode)) + ylab("Total investment (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_bar(stat='identity', position='identity', show.legend=FALSE)
 
 #10 highest investements
-highest_investments <- head(Budget_per_department_2014[order(Budget_per_department_2014$Investment_budget, decreasing=TRUE), ], 10)
-ggplot(data=highest_investments, aes(x=reorder(DepCode, -Investment_budget), y=Investment_budget, fill=DepName)) + geom_col(position = 'dodge')
+highest_investments <- head(Budget_per_department_2015[order(Budget_per_department_2015$Depensestotales, decreasing=TRUE), ], 10)
+ggplot(data=highest_investments, aes(x=reorder(DepCode, -Depensestotales), y=Depensestotales, fill=DepName)) + ylab("Total investment (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_col(position = 'dodge')
 
 #10 lower investments
-lower_investments <- tail(Budget_per_department_2014[order(Budget_per_department_2014$Investment_budget, decreasing=TRUE), ], 10)
+lower_investments <- tail(Budget_per_department_2015[order(Budget_per_department_2015$Investment_budget, decreasing=TRUE), ], 10)
 ggplot(data=lower_investments, aes(x=reorder(DepCode, Investment_budget), y=Investment_budget, fill=DepName)) + geom_col(position = 'dodge')
 
 ## BE CAREFUL !!! WE DO NOT HAVE DATA FOR THESE DEPARTMENTS
 View(Unknown_departments)
 
 #Remove department 54 which have a huge difference with the others
-Budget_per_department_2014 <- Budget_per_department_2014[-c(which(Budget_per_department_2014$DepCode == 54)), ]
-ggplot(data=Budget_per_department_2014, aes(x = DepCode, y = Investment_budget, fill=DepCode)) + geom_bar(stat='identity', position='identity', show.legend=FALSE)
+Budget_per_department_2015 <- Budget_per_department_2015[-c(which(Budget_per_department_2015$DepCode == 54)), ]
+ggplot(data=Budget_per_department_2015, aes(x = DepCode, y = Investment_budget, fill=DepCode)) + geom_bar(stat='identity', position='identity', show.legend=FALSE)
