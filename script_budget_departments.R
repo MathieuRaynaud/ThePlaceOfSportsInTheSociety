6bd6d42fd46212b7e0a6d201ffd556f98f08c02e
MathieuRaynaud <mathieu-006@hotmail.fr>
2018-12-21 12:23:47 +0100

    Finished to study investment

--- script_budget_departments.R
+++ script_budget_departments.R
@@ -9,19 +9,12 @@ Budget_per_department_2015 = read.csv("/Users/Mathieu/Documents/INSA/5ème année/
 Budget_per_department_2015 <- merge(Budget_per_department_2015, Population_per_department_2015, by = "DepName")
 
 #Plot investment per department
-ggplot(data=Budget_per_department_2015, aes(x = DepCode, y = Depensestotales, color=DepCode)) + ylab("Total investment (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_bar(stat='identity', position='identity', show.legend=FALSE)
+ggplot(data=Budget_per_department_2015, aes(x = DepCode, y = Depensestotales_hab, color=DepCode)) + ylab("Total investment per inhabitant (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_bar(stat='identity', position='identity', show.legend=FALSE)
 
 #10 highest investements
-highest_investments <- head(Budget_per_department_2015[order(Budget_per_department_2015$Depensestotales, decreasing=TRUE), ], 10)
-ggplot(data=highest_investments, aes(x=reorder(DepCode, -Depensestotales), y=Depensestotales, fill=DepName)) + ylab("Total investment (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_col(position = 'dodge')
+highest_investments <- head(Budget_per_department_2015[order(Budget_per_department_2015$Depensestotales_hab, decreasing=TRUE), ], 10)
+ggplot(data=highest_investments, aes(x=reorder(DepCode, -Depensestotales_hab), y=Depensestotales_hab, fill=DepName)) + ylab("Total investment per inhabitant (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_col(position = 'dodge')
 
-#10 lower investments
-lower_investments <- tail(Budget_per_department_2015[order(Budget_per_department_2015$Investment_budget, decreasing=TRUE), ], 10)
-ggplot(data=lower_investments, aes(x=reorder(DepCode, Investment_budget), y=Investment_budget, fill=DepName)) + geom_col(position = 'dodge')
-
-## BE CAREFUL !!! WE DO NOT HAVE DATA FOR THESE DEPARTMENTS
-View(Unknown_departments)
-
-#Remove department 54 which have a huge difference with the others
-Budget_per_department_2015 <- Budget_per_department_2015[-c(which(Budget_per_department_2015$DepCode == 54)), ]
-ggplot(data=Budget_per_department_2015, aes(x = DepCode, y = Investment_budget, fill=DepCode)) + geom_bar(stat='identity', position='identity', show.legend=FALSE)
+#10 lowest investments
+lowest_investments <- tail(Budget_per_department_2015[order(Budget_per_department_2015$Depensestotales_hab, decreasing=TRUE), ], 10)
+ggplot(data=lowest_investments, aes(x=reorder(DepCode, Depensestotales_hab), y=Depensestotales_hab, fill=DepName)) + ylab("Total investment per inhabitant (???)") + xlab("Department code") + labs(fill = "Departments names") + geom_col(position = 'dodge')
